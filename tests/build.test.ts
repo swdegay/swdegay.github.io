9b62b58e810b7c2028cf6a38dae7d77e7635e6d0
Seth Degay <mail@sethdegay.dev>
2026-06-03 21:27:22 +0800

    add font

--- tests/build.test.ts
+++ tests/build.test.ts
@@ -9,7 +9,7 @@ import { getTimestamp } from '@/utils/time.ts';
 Deno.test('should produce consistent builds with the same seed', async () => {
   const timestamp = Temporal.Instant.from('1970-01-01T00:00:00Z');
   const expected =
-    '17f7c573df0302b2e0208fd10eba4d852d35082733fe6b155d465623ee21153e';
+    '868de1d54daf5155b711bb64496e57b9b81ce472ddf9d7d01c17ace852008111';
   const commitHash = '610dac2fabc687ae9ea6bb65e6cd89e55350c992';
   const lightModeIcon = optimizeSvgInternal(
     await Deno.readTextFile('./src/light_mode.svg'),
